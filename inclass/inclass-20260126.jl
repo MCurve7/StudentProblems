480a07f4ed904f4799f62eb5466a91200fdcf55a
MCurve7 <mcurve7@gmail.com>
2026-02-02 16:56:05 -0600

    inclass 20260126 has code + new file

---
+++ inclass/inclass-20260126.jl
@@ -0,0 +1,44 @@
+1+2
+7-5
+2*3
+4/2
+4 ÷ 2 # \div TAB to get the symbol
+5 ÷ 2
+div(5,2) 
+2^3
+(1+2)*5
+1/2
+1/3
+1/2 + 1/3
+
+1//2
+1//2 + 1//3
+1/2 + 1//3
+
+π # \pi TAB to get the symbol
+
+sqrt(2)
+√2 # \sqrt TAB 2 to get the symbol
+2^0.5
+
+cbrt(8)
+8^(1/3)
+
+exp(1)
+e = exp(1)
+
+log(e)
+log(10)
+log(10, 100)
+
+M = [1 2 3; 4 5 6]
+F = [0.1 0.2; 0.3 1.4]
+R = [1/1 1/2; 1/3 1/4]
+R = [1//1 1//2; 1//3 1//4]
+
+F + R
+M + F
+
+str = "This is a string."
+str2 = "This is also a string"
+str * str2
